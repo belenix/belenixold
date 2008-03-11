c3578d6590eb25e0561f8361d6ffbdbb680ccf24
moinakg <moinakg@7ccbaa1a-f244-0410-8861-bfa69f52a080>
2008-03-11 17:34:14 +0000

    * apps/belenix_constructor/* 	  Add BeleniX Constructor toolkit - still work in progress

---
+++ apps/belenix_constructor/src/bootcd_skel/etc/default_env.sh
@@ -0,0 +1,24 @@
+PATH=/sbin:/usr/sbin:/usr/bin:/usr/ccs/bin:/usr/X11R6/bin:/usr/openwin/bin:/opt/DTT/bin
+export PATH
+
+LD_LIBRARY_PATH=/lib:/usr/lib:/usr/sfw/lib:/usr/X11R6/lib:/usr/openwin/lib
+export LD_LIBRARY_PATH
+
+MANPATH=/usr/man:/usr/share/man:/usr/X11R6/man:/usr/openwin/man:/opt/DTT/Man
+export MANPATH
+
+PKG_CONFIG_PATH=/usr/X11/lib/pkgconfig:/usr/lib/pkgconfig:/usr/sfw/lib/pkgconfig
+export PKG_CONFIG_PATH
+
+TMPDIR=/tmp
+export TMPDIR
+
+KDEVARTMP=/tmp
+export KDEVARTMP
+
+G_BROKEN_FILENAMES=yes
+export G_BROKEN_FILENAMES
+
+SSH_AGENT_EXE=/usr/bin/ssh-agent
+export SSH_AGENT_EXE
+
