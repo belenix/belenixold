4335470bba5e0f18b7298f512a790512c5a4b335
moinakg <moinakg@7ccbaa1a-f244-0410-8861-bfa69f52a080>
2009-08-13 14:06:40 +0000

    New branding package with KDE4 specifics.

---
+++ misc/belenix-branding2/profile.d/env.sh
@@ -0,0 +1,44 @@
+SHELL=/usr/bin/bash
+export SHELL
+
+PATH=/sbin:/usr/sbin:/usr/bin:/usr/ccs/bin:/usr/X11/bin:/usr/openwin/bin:/opt/DTT/bin
+export PATH
+
+#LD_LIBRARY_PATH=/lib:/usr/lib:/usr/sfw/lib:/usr/X11/lib:/usr/foss/lib:/usr/openwin/lib
+#export LD_LIBRARY_PATH
+
+MANPATH=/usr/man:/usr/share/man:/usr/gnu/share/man:/usr/X11/man:/opt/DTT/Man:/usr/sfw/share/man
+export MANPATH
+
+PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/X11/lib/pkgconfig:/usr/gnu/lib/pkgconfig:/usr/sfw/lib/pkgconfig
+export PKG_CONFIG_PATH
+
+TMPDIR=/tmp
+export TMPDIR
+
+KDEVARTMP=/tmp
+export KDEVARTMP
+
+G_BROKEN_FILENAMES=yes
+export G_BROKEN_FILENAMES
+
+#LD_PRELOAD=/usr/foss/lib/preloadable_libiconv.so
+#export LD_PRELOAD
+
+QTDIR=/usr
+export QTDIR
+
+QMAKESPEC=${QTDIR}/mkspecs/solaris-g++
+export QMAKESPEC
+
+SASL_PATH=/usr/gnu/lib/sasl2
+export SASL_PATH
+
+SSH_AGENT_EXE=/usr/bin/ssh-agent
+export SSH_AGENT_EXE
+
+XFCE4_SESSION_EXE=/usr/bin/xfce4-session
+export XFCE4_SESSION_EXE
+
+XDG_DATA_DIRS=/usr/share
+export XDG_DATA_DIRS
